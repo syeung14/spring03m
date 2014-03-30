eb9f491f113ff4358f70b9b6c9b60526cad800d4
iampolo <yeungsangsin@gmail.com>
2014-03-30 10:09:58 -0700

    spring and maven startup
    mroe info in Java\Servlet_Jsp_Struts_Ajax\tips&tutorial\maven

---
+++ src/main/java/spring03/spring03a/App.java
@@ -0,0 +1,13 @@
+package spring03.spring03a;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
